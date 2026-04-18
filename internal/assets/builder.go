182dd09ca421cd1696729a0d1f002e62fb5e2ac7
Vadim Kent <vadimkent@gmail.com>
2026-04-18 01:18:32 -0300

    style(assets): filter row justify_items bottom -> center

--- internal/assets/builder.go
+++ internal/assets/builder.go
@@ -88,7 +88,7 @@ func buildFilter(params ListParams, lang string) components.Component {
 		},
 	)
 	row := components.Row("assets-filter-row", []string{"240px", "1fr", "auto"}, sel, filler, newBtn)
-	row.Props["justify_items"] = "bottom"
+	row.Props["justify_items"] = "center"
 	return row
 }
 
